ebef2bedb4129591a6423b20efd674ac7bdf50f0
ONETEAM\DaneBurge <daneburge@truckstop.com>
2025-03-23 17:39:46 -0600

    WIP draft out HTML. Working on JS side

---
+++ app.js
@@ -0,0 +1,56 @@
+let bank = 100
+
+const players = [
+  { teamNumber: 1, emoji: '🏃‍♂️', skill: 10, name: "D'Marcus Williums" },
+  { teamNumber: 1, emoji: '🤾‍♂️', skill: 30, name: "Tyroil Smoochie-Wallace" },
+  { teamNumber: 1, emoji: '🏇', skill: 88, name: "Jackmerius Tacktheratrix" },
+  { teamNumber: 1, emoji: '🏌️‍♀️', skill: 15, name: "Javaris Jamar Javarison-Lamar" },
+  { teamNumber: 1, emoji: '🏋️‍♂️', skill: 77, name: "D'Pez Poopsie" },
+  { teamNumber: 1, emoji: '🏌️‍♂️', skill: 21, name: "D'Jasper Probincrux III" },
+  { teamNumber: 1, emoji: '🤾', skill: 5, name: "Leoz Maxwell Jilliumz" },
+  { teamNumber: 1, emoji: '🏂', skill: 99, name: "Hingle McCringleberry" },
+  { teamNumber: 1, emoji: '🧘‍♀️', skill: 50, name: "L'Carpetron Dookmarriot" },
+  { teamNumber: 1, emoji: '🚶‍♀️', skill: 1, name: "Xmus Jaxon Flaxon-Waxon" },
+  { teamNumber: 2, emoji: '🏋️‍♀️', skill: 61, name: "Saggitariutt Jefferspin" },
+  { teamNumber: 2, emoji: '🤺', skill: 34, name: "Quatro Quatro" },
+  { teamNumber: 2, emoji: '🏄', skill: 71, name: "X-Wing @Aliciousness" },
+  { teamNumber: 2, emoji: '🧜‍♂️', skill: 76, name: "Bisquiteen Trisket" },
+  { teamNumber: 2, emoji: '🤸', skill: 47, name: "Scoish Velociraptor Maloish" },
+  { teamNumber: 2, emoji: '⛹️‍♀️', skill: 23, name: "Donkey Teeth" },
+  { teamNumber: 2, emoji: '🕴️', skill: 58, name: "T.J. A.J. R.J. Backslashinfourth V" },
+  { teamNumber: 2, emoji: '💃', skill: 99, name: "Firstname Lastname" },
+  { teamNumber: 2, emoji: '🧍‍♂️', skill: 3, name: "Dan Smith" },
+  { teamNumber: 2, emoji: '🐅', skill: 100, name: "Tiger" },
+]
+
+
+function drawTeam1() {
+    const team1Container = document.getElementById('teamOne');
+    let team1Content = ''; // Create empty  container first
+
+    const team1Players = players.filter(player => player.teamNumber == 1);
+    // team1Players.forEach(player => {
+    //     team1Content += `<div class="player">${player.emoji} ${player.name}</div>`;
+    // });
+    // team1Container.innerHTML = team1Content;
+    team1Players.forEach(player => team1Content += player.emoji);
+    team1Container.innerText = team1Content;
+    //.. loop through players adding only those on team1
+  }
+
+  function draftPlayers() {
+    players.forEach(player => {
+      const randomTeamNumber = Math.ceil(Math.random() * 2)
+      player.teamNumber = randomTeamNumber
+    })
+    drawTeamOne()
+  }
+
+
+  function drawTeamOne() {
+    let teamOneEmojis = ''
+    const teamOnePlayers = players.filter(player => player.teamNumber == 1)
+    teamOnePlayers.forEach(player => teamOneEmojis += player.emoji)
+    const teamOneElement = document.getElementById('teamOneRoster')
+    teamOneElement.innerText = teamOneEmojis
+  }
